3acaf82392bbe59894114debd6759230a8c22d7f
Ricardo Rodriguez <rick.rodriguez8080@gmail.com>
2020-01-10 04:24:28 +0000

    First update

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime #added DateTime to boilerplate
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,12 +8,72 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
+
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    image = Column(String(500)) # Path/URL to image file
+    login_status = Column(String(250), nullable=False)
+    registerDate = Column(DateTime)
+
+class Post(Base):
+    __tablename__ = 'posts'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    image = Column(String(500)) # Path/url to image file
+    date = Column(DateTime)
+    location = Column(String(250))
+
+class Comment(Base):
+    __tablename__ = 'comments'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    comment = Column(String(250), nullable=False)
+    date = Column(DateTime)
+
+class Hashtag(Base):
+    __tablename__ = 'hashtag'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    comment_id = Column(Integer, ForeignKey('comments.id'))
+    
+
+class Like(Base):
+    __tablename__ = 'likes'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    photo_id = Column(ForeignKey('posts.image'))
+    comment_id = Column(ForeignKey('comments.comment'))
+
+class Follower(Base):
+    __tablename__ = 'followers'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    can_follow = Column(Integer)
+
+
+"""
 class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+
 
 class Address(Base):
     __tablename__ = 'address'
@@ -28,6 +88,7 @@ class Address(Base):
 
     def to_dict(self):
         return {}
+"""
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
